780bba95d563df971eeeebc56f0746881b64502c
yanliu1111 <liuyan0504@gmail.com>
2023-03-26 16:08:56 -0600

    feat: update user info

---
+++ update.py
@@ -0,0 +1,10 @@
+from main import User, Session, engine
+
+local_session = Session(bind=engine)
+
+user_to_update = local_session.query(User).filter(User.username=='Jana').first()
+
+user_to_update.username = 'Janaswain'
+user_to_update.email = 'janaswain@company.com'
+
+local_session.commit()
