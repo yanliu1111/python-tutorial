1f5d429a02ee243cd2f0de716c9825355c87a21a
yanliu1111 <liuyan0504@gmail.com>
2023-03-27 02:20:23 -0600

    Feat: One to Many_ creating blog table

---
+++ main.py
@@ -0,0 +1,49 @@
+# one to Many
+import os
+from sqlalchemy import (create_engine, Integer, String, Column, ForeignKey)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+BASE_DIR =os.path.dirname(os.path.realpath(__file__))
+conn = 'sqlite:///'+os.path.join(BASE_DIR,'blog.db')
+
+engine = create_engine(conn,echo=True)
+#specify echo=true meaning that we want to get all our generated SQL statements printed out to the console
+
+Base  = declarative_base()
+
+"""
+class User:
+    id: int pk
+    username: str
+    email: str
+
+class Post:
+    id: int pk
+    title: str
+    content: str
+    user_id: int foreignkey
+"""
+
+class User(Base):
+    __tablename__='users'
+    id = Column (Integer(), primary_key=True)
+    username = Column (String(40), nullable=False)
+    email = Column (String(40), nullable=True)
+    post = relationship('Post', backref='author', cascade="all, delete")
+    # backref is finding out which user is the author of the post
+    # cascade="all, delete" means that if we delete a user, all the posts related to that user will be deleted as well
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
+class Post(Base):
+    __tablename__='posts'
+    id = Column (Integer(), primary_key=True)
+    title = Column (String(45), nullable=False)
+    content = Column (String(255), nullable=False)
+    user_id = Column (Integer(), ForeignKey('users.id', ondelete='CASCADE'))
+    # ondelete='CASCADE' means that if we delete a user, all the posts related to that user will be deleted as well
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
+    
+Base.metadata.create_all(engine)
