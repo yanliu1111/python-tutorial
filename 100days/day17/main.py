a59cf910ca63bf5a439bf2ec1c20e46b93adcf3b
yanliu1111 <liuyan0504@gmail.com>
2025-10-25 17:06:48 -0600

    update

--- 100days/day17/main.py
+++ 100days/day17/main.py
@@ -1,1 +1,10 @@
-class User: 
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
---
+++ 100days/day17/question_model.py
@@ -0,0 +1,6 @@
+class Question:
+  def __init__(self, q_text, q_answer):
+    self.text = q_text
+    self.answer = q_answer
+
+#repr means representation, it is used to define how an object is represented as a string
